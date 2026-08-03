5ca2bf90ca0e8865dae67411e4feb55af503b9ab
robv5712-lab <robv5712@gmail.com>
2026-08-02 21:20:27 -0700

    Add files via upload
    update for icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE = "park-tracker-v2";
-var ASSETS = [".", "index.html", "manifest.json"];
+var CACHE = "park-tracker-v3";
+var ASSETS = [".", "index.html", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); }));
